657e9e557197b6214e41a8bd3f37b7a35da72cb4
jinsung44638 <hoyajs77@gmail.com>
2025-09-10 14:46:36 +0900

    display1, 2 , footer 작성중

---
+++ js/display-1.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function () {
+  var swiper = new Swiper(".display1Swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true, // ✅ 가운데 정렬
+    slidesPerView: 2, // ✅ 3장만 보이게
+spaceBetween: 30,   
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 2.5,
+      slideShadows: true,
+    },
+    loop: true, // 무한 반복
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+});
